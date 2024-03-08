7d212a8338fba4d7016785539923f5c512d58b71
argonarch <armando12123434@gmail.com>
2024-03-08 16:43:46 -0300

    fix time

--- facturador_impuestos/mail.py
+++ facturador_impuestos/mail.py
@@ -8,19 +8,19 @@ import env
 
 now = datetime.datetime.now()
 print(now)
-fecha = datetime.date(now.year, now.month, 1)
+fecha = datetime.date(now.year, now.month-1, 1)
 
 def buscador(servicio):
     print('Iniciando') 
     with MailBox(env.host).login(env.username, env.password) as mailbox:
         print('Edesur')
-        for msg in mailbox.fetch(A(from_=env.mail_edesur, subject=env.asunto_edesur, date_gte=env.fecha)):
+        for msg in mailbox.fetch(A(from_=env.mail_edesur, subject=env.asunto_edesur, date_gte=fecha)):
             filter('Edesur', msg.html, msg.date, "[0-9]{2}/[0-9]{2}/[0-9]{4}", "\$<strong>([0-9]{1,}\.[0-9]{2})")
         print('Metrogas')
-        for msg in mailbox.fetch(A(from_=env.mail_metrogas, subject=env.asunto_metrogas, date_gte=env.fecha)):
+        for msg in mailbox.fetch(A(from_=env.mail_metrogas, subject=env.asunto_metrogas, date_gte=fecha)):
             filter('Metrogas', msg.html, msg.date, ": ([0-9]{2}\.[0-9]{2}\.[0-9]{4})", "\$.([0-9]{1,}\.[0-9]{1,}\,[0-9]{2})")
         print('Aysa')
-        for msg in mailbox.fetch(A(from_=env.mail_aysa, subject=env.asunto_aysa, date_gte=env.fecha)):
+        for msg in mailbox.fetch(A(from_=env.mail_aysa, subject=env.asunto_aysa, date_gte=fecha)):
             filter('Aysa', msg.html, msg.date, "[0-9]{2}/[0-9]{2}/[0-9]{4}", "\$.([0-9]{1,}\.[0-9]{1,}\,[0-9]{2})")
 
 
