5edf870efadc2087023ced6df8e535c387a35038
argonarch <armando12123434@gmail.com>
2024-03-08 17:35:53 -0300

    fix

--- facturador_impuestos/messenger.py
+++ facturador_impuestos/messenger.py
@@ -1,11 +1,9 @@
 import requests
-
+import env
 new_line = '\n'
 
-def get_url(msg):
-    my_token = '6049411213:AAFzNRdrgejNj0HMqjJ_t5t593ZDySSw7LI'
-    chat_id = '-1001477006866'
-    return f"https://api.telegram.org/bot{my_token}/sendMessage?chat_id={chat_id}&text={msg}"
+def get_url(msg): 
+    return f"https://api.telegram.org/bot{env.my_token}/sendMessage?chat_id={env.chat_id}&text={msg}"
 
 def sendMessages(tittle, price, expiration, timeCheck):
     msgBody = f'Factura {tittle} {new_line} Emision: {timeCheck} {new_line} Precio: {price} {new_line} Vencimiento: {expiration}'
