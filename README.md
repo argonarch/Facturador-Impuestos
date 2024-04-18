2158343d3a92842f5fe832cd76afe1ade12125d4
argonarch <armando12123434@gmail.com>
2024-04-18 05:19:52 -0300

    add readme change poetry package manager to conda remove peotry files add environment conda

---
+++ README.md
@@ -0,0 +1,10 @@
+# Facturador de Impuestos
+Una simple aplicacion que detecta los vencimientos y saldo a pagar de los servicios:
+
+- Aysa
+- Metrogas
+- Edesur
+
+Mediante las notificacion por mail.
+
+Finalmente envia por telegram los datos correspondientes.
---
+++ src/env.py
@@ -0,0 +1,18 @@
+# enable less secure apps on your google account
+# https://myaccount.google.com/lesssecureapps
+host = "imap.gmail.com"
+
+username = "jorgemarceloescobar@gmail.com"
+password = "jgbxcbfuyeqnyyef"
+
+mail_edesur = "noresponder@facturas.edesur.com.ar"
+asunto_edesur = "Factura EDESUR disponible on-line"
+
+mail_metrogas = "no-reply@metrogas.com.ar"
+asunto_metrogas = "Factura MetroGas"
+
+mail_aysa = "avisos@aysadigital.com.ar"
+asunto_aysa = "Aviso de Factura Digital Disponible "
+
+my_token = '6049411213:AAFzNRdrgejNj0HMqjJ_t5t593ZDySSw7LI'
+chat_id = '-1001477006866'
